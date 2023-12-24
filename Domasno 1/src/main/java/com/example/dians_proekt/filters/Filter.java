d597823ed4a00d4dc7436f3742c2995a7e1dd4a4
MonikaJov <86190069+MonikaJov@users.noreply.github.com>
2023-12-24 13:13:33 +0100

    left only Domasno 3 in new branch

--- Domasno 1/src/main/java/com/example/dians_proekt/filters/Filter.java
+++
@@ -1,5 +0,0 @@
-package com.example.dians_proekt.filters;
-
-public interface Filter<I, O>{
-    O execute(I i);
-}
--- Domasno 1/src/main/java/com/example/dians_proekt/filters/RemoveSignFilter.java
+++
@@ -1,8 +0,0 @@
-package com.example.dians_proekt.filters;
-
-public class RemoveSignFilter implements Filter<String,String>{
-    @Override
-    public String execute(String s) {
-        return s.replace(">", "");
-    }
-}
